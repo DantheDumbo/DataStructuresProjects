f0677cdfb860e02d2e9c611eb903ba1707110c19
DantheDumbo <daniel.tuma.001@gmail.com>
2024-11-18 13:07:51 -0500

    Your descriptive message

--- MazeProject/src/Maze.java
+++ MazeProject/src/Maze.java
@@ -1,5 +1,34 @@
+import java.util.Stack;
+
 public class Maze {
-    private static Mouse() {
-        
+    public enum Direction {NORTH, EAST, SOUTH, WEST}
+
+    private int rows;
+    private int cols;
+
+    private int footX;
+    private int footY;
+    private Stack<Mouse> stack; 
+
+    private int[][] maze;
+    private boolean [][] visited;
+
+    private static class Mouse {
+        int x, y;
+        Direction dir;
+
+        public Mouse(int x, int y, Direction dir) {
+            this.y = y;
+            this.x = x;
+            this.dir = dir;
+        }
+    }
+
+    public Maze(int rows, int cols, int[][] values, int foodX, int foodY) {
+
+    }
+
+    private void initializeVisited() {
+        // set all values of the visited matrix to false
     }
 }
