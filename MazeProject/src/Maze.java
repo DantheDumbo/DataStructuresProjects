8a614efafc8317ef037d4e0ae18f31f89e306626
DantheDumbo <daniel.tuma.001@gmail.com>
2024-11-21 09:16:53 -0500

    Your descriptive message

--- MazeProject/src/Maze.java
+++ MazeProject/src/Maze.java
@@ -8,10 +8,12 @@ public class Maze {
 
     private int footX;
     private int footY;
-    private Stack<Mouse> stack; 
+    private Stack<Mouse> stack;
+
+    private boolean isSolved;
 
     private int[][] maze;
-    private boolean [][] visited;
+    private boolean[][] visited;
 
     private static class Mouse {
         int x, y;
@@ -25,10 +27,21 @@ public class Maze {
     }
 
     public Maze(int rows, int cols, int[][] values, int foodX, int foodY) {
+        this.isSolved = false;
+        initializeVisited();
+    }
 
+    //checks to see if four conditions have beeen met
+    private boolean isValid(int row, int col) {
+        return true; // temp
+    }
+
+    private boolean wasVisited(int row, int col) {
+        return true; // temp
     }
 
     private void initializeVisited() {
         // set all values of the visited matrix to false
     }
+
 }
--- StacksProject/src/MyStack.java
+++ StacksProject/src/MyStack.java
@@ -1,3 +1,38 @@
 public class MyStack {
-    
+
+    private int[] stack;
+    int pointer;
+
+    public MyStack() {
+        stack = new int[10];
+        pointer = 0;
+    }
+
+    public void push(int num) {
+        if (pointer < 9) {
+            stack[pointer] = num;
+            pointer++;
+        }
+    }
+    public int pop() {
+        pointer--;
+        return stack[pointer];
+    }
+    public int peak() {
+        return stack[pointer-1];
+    }
+    public int size() {
+        return pointer;
+    }
+    public boolean isEmpty() {
+        return pointer != 9;
+    }
+    public boolean isFull() {
+        return pointer == 9;
+    }
+
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+         
+    }
 }
