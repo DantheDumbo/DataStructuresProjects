80ad206d52fec4c4d23108696ef5aef119764e5d
DantheDumbo <daniel.tuma.001@gmail.com>
2024-11-21 09:49:08 -0500

    Your descriptive message

--- StacksProject/src/MyStack.java
+++ StacksProject/src/MyStack.java
@@ -25,14 +25,59 @@ public class MyStack {
         return pointer;
     }
     public boolean isEmpty() {
-        return pointer != 9;
+        return pointer == 0;
     }
     public boolean isFull() {
         return pointer == 9;
     }
 
+    @Override
+    public String toString() {
+        String str = "";
+        for (int i=0; i<pointer; i++) {
+            str += stack[i] + "\n";
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
         MyStack stack = new MyStack();
-         
+
+        System.out.println(stack.isEmpty());
+
+        stack.push(8);
+
+        System.out.println(stack.isEmpty());
+
+        stack.push(7);
+        stack.push(0);
+        stack.push(82);
+        stack.push(1);
+
+        System.out.println(stack.peak());
+        System.out.println(stack.pop());
+        System.out.println(stack.peak());
+
+        System.out.println();
+
+        System.out.println(stack);
+
+        System.out.println(stack.size());
+
+        System.out.println();
+
+        System.out.println(stack.isFull());
+
+        stack.push(-1);
+        stack.push(-1);
+        stack.push(-1);
+        stack.push(-1);
+        stack.push(-1);
+
+        System.out.println(stack.isFull());
+
+        System.out.println(stack);
+
+        System.out.println(stack.size());
     }
 }
