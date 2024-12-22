9792941fdf53d15c41246d760fa1dcb473e2e12e
DantheDumbo <daniel.tuma.001@gmail.com>
2024-12-21 19:28:48 -0500

    Finished Massada

--- Massada/src/Main.java
+++ Massada/src/Main.java
@@ -1,8 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
-        for (int i=0; i <100; i++) {
+import java.util.*;
 
+public class Main {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
 
+        @Override
+        public String toString() {
+            return data + "";
+        }
+    }
+
+    public static void main(String[] args) {
+        int numPeople = 0;
+        int lastPersonStanding = 0;
+        Node temp = null;
+        Node front = null;
+
+        for (int i=1; i <= 256; i++) {
+            numPeople = i;
+            front = new Node(1);
+            temp = front;
+
+            for (int j = 2; j <= i; j++) {
+                temp.next = new Node(j);
+                temp = temp.next;
+            }
+            temp.next = front;
+            temp = front;
+
+            while (numPeople > 1) {
+                temp.next = temp.next.next;
+                temp = temp.next;
+                --numPeople;
+            }
+            System.out.println("Person " + temp + " survived with " + i + " people. ");
+        }
     }
 }
