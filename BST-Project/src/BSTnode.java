254456514f6756327cdf0278cce9896407bdda0d
DantheDumbo <daniel.tuma.001@gmail.com>
2025-02-02 21:53:48 -0500

    BST-Project

---
+++ BST-Project/src/BSTnode.java
@@ -0,0 +1,16 @@
+public class BSTnode {
+    BSTnode left;
+    int data;
+    BSTnode right;
+
+    @Override
+    public String toString() {
+        return data + "";
+    }
+
+    public BSTnode(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = nullc;
+    }
+}
---
+++ BST-Project/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static BSTnode setRoot(BSTnode r) {
+        return new BSTnode(-5);
+    }
+
+    public static void main(String[] args) {
+        BSTnode root = null;
+         root = setRoot(root);
+        System.out.println(root);
+    }
+}
