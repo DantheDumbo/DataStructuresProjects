cfc7904e3e5732dadb51b09b6638769504a4e912
DantheDumbo <daniel.tuma.001@gmail.com>
2025-02-20 21:40:06 -0500

    yam

--- Homebrew Graph/src/Graph.java
+++ Homebrew Graph/src/Graph.java
@@ -11,34 +11,34 @@ public class Graph {
             this.label = label;
         }
     }
-        private Map<Node, List<Node>> adjVertices;
+        private Map<Node, List<Node>> adjNode;
 
         public Graph() {
-            adjVertices = new HashMap<>();
+            adjNode = new HashMap<>();
         }
 
         public void addNode(Node Node) {
-            adjVertices.putIfAbsent(Node, new ArrayList<>());
+            adjNode.putIfAbsent(Node, new ArrayList<>());
         }
 
         public void addEdge(Node Node1, Node Node2) {
-            adjVertices.get(Node1).add(Node2);
-            adjVertices.get(Node2).add(Node1); // For undirected graph
+            adjNode.get(Node1).add(Node2);
+            adjNode.get(Node2).add(Node1); // For undirected graph
         }
 
         public void removeEdge(Node Node1, Node Node2) {
-            adjVertices.get(Node1).remove(Node2);
-            adjVertices.get(Node2).remove(Node1); // For undirected graph
+            adjNode.get(Node1).remove(Node2);
+            adjNode.get(Node2).remove(Node1); // For undirected graph
         }
 
         public void removeNode(Node Node) {
-            adjVertices.remove(Node);
-            for (List<Node> edges : adjVertices.values()) {
+            adjNode.remove(Node);
+            for (List<Node> edges : adjNode.values()) {
                 edges.remove(Node);
             }
         }
 
         public List<Node> getVertices() {
-            return new ArrayList<>(adjVertices.keySet());
+            return new ArrayList<>(adjNode.keySet());
         }
 } 
