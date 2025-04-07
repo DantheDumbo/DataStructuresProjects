a67f9d2f20d532d926b9f325b396897ec8c039ae
DantheDumbo <daniel.tuma.001@gmail.com>
2025-04-07 12:28:55 -0400

    Started Quicksort

---
+++ Quicksort/src/Quicksort.java
@@ -0,0 +1,26 @@
+public class Quicksort {
+
+    public int getPivot(int[] arr) {
+
+        int a = arr[0];
+        int b = arr[arr.length / 2];
+        int c = arr[arr.length - 1];
+
+        if ((a > b) != (a > c)) return a;
+        else if ((b > a) != (b > c)) return b;
+        else return c;
+    }
+
+    public int[] quicksort(int[] arr) {
+        if (arr == null) {
+            return arr;
+        }
+        int pivot = getPivot(arr);
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,8,2,4,5,0,2,2,7};
+    }
+}
