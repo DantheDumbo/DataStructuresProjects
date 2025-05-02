3bbc41b3b485e64bbe1df680620cb1393d96106b
DantheDumbo <daniel.tuma.001@gmail.com>
2025-05-02 12:51:47 -0400

    save

--- Problem/src/Main.java
+++ Problem/src/Main.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Employee[] db = new Employee[10];
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Coroll", 42),
+                new Person("Thomas", "Carlye", 51),
+                new Person("Charlotte", "Dickens", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
 
-        for (Employee e : db) {
-            if (e.getLast.chatAt(0) == 'C') {
+        // Step 1: Sort list by last name
 
-            }
+        // Step 2: Create a method that prints all elements in list
+
+        // Step 3: Create a method that prints all people that have last name beginning with 'C'
+
+        private static void printLastNameBegginingWithC(List<Person> people, Condition condition) {
+
         }
     }
 }
