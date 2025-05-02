eb4433fe0e4262ea2d31db3eb3b6cb838e0e2eb5
DantheDumbo <daniel.tuma.001@gmail.com>
2025-05-02 11:59:01 -0400

    csa shizzzzz

---
+++ Problem/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee[] db = new Employee[10];
+
+        for (Employee e : db) {
+            if (e.getLast.chatAt(0) == 'C') {
+
+            }
+        }
+    }
+}
